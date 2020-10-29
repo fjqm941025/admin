e8609b8588924b7f0932c3ae5b4b02c7d2f1dbbc
fjqm941025 <fjqm941025@naver.com>
2020-10-29 23:29:33 +0900

    delete

--- project_1/project/static/create.js
+++
@@ -1,9 +0,0 @@
-function xml_create(){
-    let url = 'http://localhost:3001';
-    const str = "hihi";
-
-};
-
-
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", xml_create);
